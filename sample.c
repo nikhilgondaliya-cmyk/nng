8486e294d1f477dac9cebb4daf57a54b9a1d8105
nikhil <nikhil.gondaliya@gmail.com>
2026-08-19 20:15:07 +0530

    new commit

--- sample.c
+++ sample.c
@@ -5,10 +5,13 @@ void main()
 printf("Hello");
 printf("hi");
 printf("Hi from Github");
-printf("hi"); 
+printf("hi NIKHIL GONDALIYA"); 
 printf("hello");
 printf("Hi from sng branch");
 printf("hello hi");
 printf("Hi from desktop");
 printf("from github");
+printf("changes at 7:30 pm");
+printf("staging area");
+printf("hi");
 }
